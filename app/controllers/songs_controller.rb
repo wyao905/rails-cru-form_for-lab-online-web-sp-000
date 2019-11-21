ae431af4ae1fd95993fedac3a957b5d4a2892514
Warren Yao <warrenyca905@gmail.com>
2019-11-21 02:44:50 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,5 +8,6 @@ class SongsController < ApplicationController
   end
   
   def create
-    @song = Song.new(song_params[:name, ])
+    @song = Song.new(song_params(:name, :artist_id, :genre_id))
+    
 end
