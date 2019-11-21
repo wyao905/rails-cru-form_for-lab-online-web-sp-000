6f8d75811873b1a80c060fb7be2f5ed3b463e898
Warren Yao <warrenyca905@gmail.com>
2019-11-21 02:45:37 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,5 +9,13 @@ class SongsController < ApplicationController
   
   def create
     @song = Song.new(song_params(:name, :artist_id, :genre_id))
-    
+    @song.save
+    redirect_to song_path(@song)
+  end
+  
+  private
+  
+  def song_params(*args)
+    params.required(:song).permit(*args)
+  end
 end
