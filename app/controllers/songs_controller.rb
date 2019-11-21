03435c1bccc5ea354a49689e6d2f3a5902718b17
Warren Yao <warrenyca905@gmail.com>
2019-11-21 02:44:17 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,4 +2,11 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
   end
+  
+  def new
+    @song = Song.new
+  end
+  
+  def create
+    @song = Song.new(song_params[:name, ])
 end
